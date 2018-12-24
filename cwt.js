8d4efce92810f56eac6d8623bc547b914cda7c72
Aria Pahlavan <apahlavan1@utexas.edu>
2018-12-24 12:20:00 -0600

    Fix bug where async token generations and data retrivals were blocking

--- cwt.js
+++ cwt.js
@@ -3,6 +3,18 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET_KEY = 'shhhh...';
 const id = 1234;
 
+const runAsync = (codeblock) => {
+  setTimeout(codeblock, 0);
+}
+
+const wait = () => {
+  let y = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    y = i;
+  }
+}
+
+
 const generateToken = (data) => {
   if (data === null || data === undefined) {
     throw Error('Invalid data');
@@ -12,30 +24,34 @@ const generateToken = (data) => {
 
 const onGeneratedToken = (data) => {
   const subscribe = (onSuccess, onError) => {
-    if (data === null || data === undefined) {
-      return onError('Invalid data');
-    }
-    try {
-      const token = jwt.sign({data}, JWT_SECRET_KEY);
-      return onSuccess(token);
-    } catch(e) {
-      return onError('Invalid data or secret key', e);
-    }
+    runAsync(() => {
+      if (data === null || data === undefined) {
+        return onError('Invalid data');
+      }
+      try {
+        const token = jwt.sign({data}, JWT_SECRET_KEY);
+        return onSuccess(token);
+      } catch(e) {
+        return onError('Invalid data or secret key', e);
+      }
+    });
   }
   return {subscribe};
 };
 
 const promiseToken = (data) => {
   return new Promise((resolve, reject) => {
-    if (data === null || data === undefined) {
-      return reject('Invalid data');
-    }
-    try {
-      const token = jwt.sign({data}, JWT_SECRET_KEY);
-      return resolve(token);
-    } catch(e) {
-      return reject('Invalid data or secret key');
-    }
+    runAsync(() => {
+      if (data === null || data === undefined) {
+        return reject('Invalid data');
+      }
+      try {
+        const token = jwt.sign({data}, JWT_SECRET_KEY);
+        return resolve(token);
+      } catch(e) {
+        return reject('Invalid data or secret key');
+      }
+    });
   });
 }
 
@@ -45,24 +61,28 @@ const retrieveData = (token) => {
 
 const onRetrievedData = (token) => {
   const subscribe = (onSuccess, onError) => {
-    try {
-      const data = jwt.verify(token, JWT_SECRET_KEY);
-      return onSuccess(data['data']);
-    } catch (e) {
-      return onError('Invalid token', e);
-    }
+    runAsync(() => {
+      try {
+        const data = jwt.verify(token, JWT_SECRET_KEY);
+        return onSuccess(data['data']);
+      } catch (e) {
+        return onError('Invalid token', e);
+      }
+    });
   }
   return {subscribe};
 };
 
 const promiseData = (token) => {
   return new Promise((resolve, reject) => {
-    try {
-      const data = jwt.verify(token, JWT_SECRET_KEY);
-      return resolve(data['data']);
-    } catch (e) {
-      return reject('Invalid token', e);
-    }
+    runAsync(() => {
+      try {
+        const data = jwt.verify(token, JWT_SECRET_KEY);
+        return resolve(data['data']);
+      } catch (e) {
+        return reject('Invalid token', e);
+      }
+    });
   });
 };
 
@@ -73,14 +93,12 @@ promiseToken(id)
 
 
 
-
-// let token;
-// onGeneratedToken(id).subscribe(
-//   result => token = result,
-//   error => console.log(error)
-// );
-//
-// onRetrievedData(token).subscribe(
-//   result => console.log('ID:', result),
-//   (msg, error) => console.log(msg)
-// );
+onGeneratedToken(id).subscribe(
+  result => {
+    onRetrievedData(result).subscribe(
+      result => console.log('ID:', result),
+      (msg, error) => console.log(msg)
+    );
+  },
+  error => console.log(error)
+);
